b967cb7a84a65dbb3ac9ad69243ce1a927a5fec4
moalwnfkxviwbdksviasbabwbsjbazsjbsissb <130096000+moalwnfkxviwbdksviasbabwbsjbazsjbsissb@users.noreply.github.com>
2023-08-14 02:45:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = 26286754
+API_HASH = "7a637e05958e14496e6ab1910a9eb9e7"
+BOT_TOKEN = "6484547301:AAEBttmjmTL42Sdz_omIJSuwx2FT2VAoWac"
+OWNER_ID = 1420974008
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
-
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = "mongodb+srv://levi303:levi303@cluster0.ws4aofd.mongodb.net/?retryWrites=true&w=majority"
+MUST_JOIN = "SIS_f"
