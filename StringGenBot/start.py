78bec10748787dca4e33ccc79ccabd5dea34c975
moalwnfkxviwbdksviasbabwbsjbazsjbsissb <130096000+moalwnfkxviwbdksviasbabwbsjbazsjbsissb@users.noreply.github.com>
2023-08-14 02:35:51 +0300

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -26,8 +26,8 @@ async def start(bot: Client, msg: Message):
                     InlineKeyboardButton(text="⚡𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄 𝐒𝐓𝐑𝐈𝐍𝐆⚡", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("❣️ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ❣️", url="https://t.me/Tg_Friendss"),
-                    InlineKeyboardButton("🥀 𝐎𝐟𝐟𝐢𝐜𝐞 🥀", url="https://t.me/VIP_CREATORS")
+                    InlineKeyboardButton("❣️ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ❣️", url="https://t.me/K_1_u"),
+                    InlineKeyboardButton("🥀 𝐎𝐟𝐟𝐢𝐜𝐞 🥀", url="https://t.me/Q_B_h")
                 ]
             ]
         ),
